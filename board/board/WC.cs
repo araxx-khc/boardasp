ee64094006ded0fb48b7f146185e458b723ca161
araxx-khc <araxx.mcfast@gmail.com>
2022-01-28 17:51:04 +0100

    commit1

---
+++ board/board/WC.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Board
+{
+    public static class WC
+    {
+        public const string ImagePath = @"\images\product\";
+        public const string SessionCart = "ShoppingCartSession";
+
+        public const string AdminRole = "Admin";
+        public const string CustomerRole = "Customer";
+
+        public const string EmailAdmin = "admin.board@gmail.com";
+        // password to admin: Admin1!
+
+    }
+}
